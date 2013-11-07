cf23e2d2bd00de923480fdc9c24d7bc58b8b4e50
Dave Lewis <dave.lewis@qmul.ac.uk>
2013-11-07 12:49:38 +0000

    Now loops over all events to get data

--- day2/dimuon/dimuons.py
+++ day2/dimuon/dimuons.py
@@ -5,4 +5,11 @@ import ROOT as r
 if __name__=='__main__':
     print "Creating histograms..."
     data_file = r.TFile("/users/lewis/GitFiles/SWC/day2/data/events.root")
-
+    tree_ = data_file.Get("events")
+    tree_.Print()
+    n_events = tree_.GetEntries()
+    for i in xrange(n_events):
+        tree_.GetEntry(i)
+        n_particles = tree_.nPart
+        
+        
